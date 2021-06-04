49accec9f7d74f5c99a9bd3908a461d65d8171e3
Mohammed Asif <asif.cse473@gmail.com>
2021-06-04 13:43:15 +0600

    topnav done

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React, {useEffect} from 'react';
 import {useState} from 'react';
-import {View, Text, SafeAreaView, ScrollView, StyleSheet} from 'react-native';
-import {Card, Button} from 'react-native-elements';
+import {View, Text, SafeAreaView, ScrollView, StyleSheet, ImageBackground} from 'react-native';
+import {Card, Button, Header} from 'react-native-elements';
 const App = () => {
   const [users, setUsers] = useState([]);
-
+  const image = { uri: "https://wallpaperaccess.com/full/2848838.jpg" };
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(res => res.json())
@@ -14,14 +14,26 @@ const App = () => {
     <SafeAreaView>
       <ScrollView>
         <View>
-          <Text style={styles.text}>User List</Text>
-          {users.map(user => (
-            <Card>
-              <Card.Title>{user.name}</Card.Title>
-              <Text>{user.email}</Text>
-              <Text>{user.address.city}</Text>
-              <Button title="Read more" />
-            </Card>
+          <Header
+            leftComponent={{ icon: 'menu', color: '#fff' }}
+            centerComponent={{ text: 'User List', style: { color: '#fff', fontSize: 30, fontWeight: 'bold'} }}
+            rightComponent={{ icon: 'home', color: '#fff' }}
+            containerStyle={{
+              backgroundColor: 'orangered',
+              justifyContent: 'space-around',
+              paddingTop: 20,
+              paddingBottom: 15
+            }}
+          />
+          {users.map((user, index) => (
+            <View style={ styles.cardStyle} key={index}>
+              <ImageBackground source={image} style={{width: 'auto', height: 'auto', padding: 10}}>
+              <Card.Title style={ styles.title}>{user.name}</Card.Title>
+              <Text style={ styles.email}>Email: {user.email}</Text>
+              <Text style={ styles.address}>Address: {user.address.street}, {user.address.city}</Text>
+              <Button title="Read more"/>
+              </ImageBackground>
+              </View>
           ))}
         </View>
       </ScrollView>
@@ -34,6 +46,29 @@ const styles = StyleSheet.create({
     fontSize: 35,
     textAlign: 'center',
   },
+  cardStyle: {
+    borderRadius: 10,
+    margin: 15,
+    padding: 20,
+    borderColor: 'orangered',
+    borderWidth: 5,
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  email: {
+    color: '#336633',
+    fontSize: 20,
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  address: {
+    fontSize: 18,
+    fontStyle: 'italic',
+    paddingBottom: 20
+  }
 });
 
 export default App;
